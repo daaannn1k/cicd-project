ee88a2743d3342568cfff7ecbc5ad50051daa68e
Vladislav Dandara <vladislav.dandara@vitamojo.com>
2024-02-15 12:55:13 +0200

    some changes in cdk.app

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,8 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'MyFirstPipeLine', {
+      pipelineName: 'MyFirstPipeLine',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('daaannn1k/cicd-project', 'master'),
+        commands: [
+          'npm ci',
+          'npx cdk synth'
+        ]
+      })
+    })
   }
 }
